7749bcebe6f1f6c30b40185be0863e7fe4db7717
jinxf0822 <jinxf0822@foxmail.com>
2019-03-25 23:29:06 +0800

    1.只出现一次的数字。2.两个数组的交集贰

--- primary_algorithms/src/main/java/array/ArrayProblem.java
+++ primary_algorithms/src/main/java/array/ArrayProblem.java
@@ -1,6 +1,8 @@
 package array;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author Jin Xiaofeng
@@ -96,11 +98,166 @@ public class ArrayProblem {
         return profit;
     }
 
+    /**
+     * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+     *
+     * 示例 1:
+     * 输入: [1,2,3,4,5,6,7] 和 k = 3
+     * 输出: [5,6,7,1,2,3,4]
+     * 解释:
+     * 向右旋转 1 步: [7,1,2,3,4,5,6]
+     * 向右旋转 2 步: [6,7,1,2,3,4,5]
+     * 向右旋转 3 步: [5,6,7,1,2,3,4]
+     *
+     * 示例 2:
+     * 输入: [-1,-100,3,99] 和 k = 2
+     * 输出: [3,99,-1,-100]
+     * 解释:
+     * 向右旋转 1 步: [99,-1,-100,3]
+     * 向右旋转 2 步: [3,99,-1,-100]
+     *
+     * 说明:
+     * 尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+     * 要求使用空间复杂度为 O(1) 的原地算法。
+     * @param nums 待操作数组
+     * @param k 右移数量
+     */
+    public void rorate(int[] nums,int k){
+        int length = nums.length;
+        int[] newNums=new int[length];
+        k=k%length;
+        for (int i = 0; i < length; i++) {
+            if(i+k>=length){
+                newNums[(i+k)%length]=nums[i];
+            }else{
+                newNums[i+k]=nums[i];
+            }
+        }
+        for (int i = 0; i < length; i++) {
+            nums[i]=newNums[i];
+        }
+    }
+
+    /**
+     * 给定一个整数数组，判断是否存在重复元素。
+     * 如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回 false。
+     *
+     * 示例 1:
+     * 输入: [1,2,3,1]
+     * 输出: true
+     *
+     * 示例 2:
+     * 输入: [1,2,3,4]
+     * 输出: false
+     *
+     * 示例 3:
+     * 输入: [1,1,1,3,3,4,3,2,4,2]
+     * 输出: true
+     *
+     * @param nums
+     * @return
+     */
+    public boolean containsDuplicate(int[] nums) {
+//        Arrays.sort(nums);
+//        for (int i = 0; i < nums.length-1; i++) {
+//            if(nums[i]==nums[i+1]){
+//                return true;
+//            }
+//        }
+//        return false;
+
+        if(nums.length < 2){
+            return false;
+        }
+        int max = nums[0];
+        for(int i = 1;i < nums.length;i++){
+            if(nums[i] > max){
+                max = nums[i];
+            }else if(nums[i] == max){
+                return true;
+            }else{
+                for (int j = i - 1;j >= 0;j--){
+                    if(nums[i] == nums[j]){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 只出现一次的数字
+     *
+     * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+     * 说明：
+     * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+     *
+     * 示例 1:
+     * 输入: [2,2,1]
+     * 输出: 1
+     *
+     * 示例 2:
+     * 输入: [4,1,2,1,2]
+     * 输出: 4
+     * @param nums
+     * @return
+     */
+    public int singleNumber(int[] nums) {
+        //TODO 学习理解位运算
+        int res = 0;
+        for (int num : nums)
+            res ^= num;
+        return res;
+    }
+
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if(nums1.length==0||nums2.length==0){
+            int[] t = new int[0];
+            return t;
+        }
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        List<Integer> list = new LinkedList<>();
+
+        int i=0;
+        int j=0;
+        int l1=nums1.length;
+        int l2=nums2.length;
+        //4,5,9
+        //4,4,8,9,9
+        while(i<l1&&j<l2){
+//            if(i>=l1){
+//                i=l1-1;
+//            }
+//            if(j>=l2){
+//                j=l2-1;
+//            }
+            if(nums1[i]==nums2[j]){
+                list.add(nums1[i]);
+                i++;
+                j++;
+            }else if(nums1[i]<nums2[j]){
+                i++;
+            }else{
+                j++;
+            }
+        }
+        int[]  result = new int[list.size()];
+        int k=0;
+        for (Integer temp :list) {
+            result[k]=temp;
+            k++;
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         ArrayProblem p = new ArrayProblem();
-        int[] a = {1,100,9,10};
-        System.out.println("初始数组，" + Arrays.toString(a));
-
-        System.out.println("返回的数字是：" + p.maxProfit(a));
+        int[] a = {4,9,5};
+        int[] b = {9,4,9,8,4};
+//        System.out.println("初始数组，" + Arrays.toString(a));
+        int[] c=p.intersect(a,b);
+        System.out.println("返回的数字是：" + Arrays.toString(c));
     }
 }
