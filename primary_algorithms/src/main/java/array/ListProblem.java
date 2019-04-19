fec2b01d6a7023ab82dc51ff60aa952d6af13673
jinxf0822 <jinxf0822@foxmail.com>
2019-04-19 21:09:06 +0800

    1翻转链表 2合并有序链表

--- primary_algorithms/src/main/java/array/ListProblem.java
+++ primary_algorithms/src/main/java/array/ListProblem.java
@@ -6,8 +6,13 @@ import java.util.LinkedList;
  * @author Jin Xiaofeng
  */
 public class ListProblem {
+    //TODO 增加单元测试
     private ListNode head = null;
 
+    /**
+     * 增加一个节点，到末尾
+     * @param x
+     */
     public void add(int x){
         ListNode newNode = new ListNode(x);
         if (head == null) {
@@ -42,7 +47,25 @@ public class ListProblem {
     }
 
 
-
+    /**
+     * 删除链表中的节点
+     * 请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点，
+     * 你将只被给定要求被删除的节点。
+     * 现有一个链表 -- head = [4,5,1,9]，它可以表示为:
+     *
+     * 示例 1:
+     * 输入: head = [4,5,1,9], node = 5
+     * 输出: [4,1,9]
+     * 解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，
+     * 该链表应变为 4 -> 1 -> 9.
+     *
+     * 示例 2:
+     * 输入: head = [4,5,1,9], node = 1
+     * 输出: [4,5,9]
+     * 解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，
+     * 该链表应变为 4 -> 5 -> 9.
+     * @param node
+     */
     public void deleteNode(ListNode node) {
         //将node下一个节点的val赋值给node。再将node.next指向
         //下下一个。实现删除该节点的功能。
@@ -50,6 +73,24 @@ public class ListProblem {
         node.next=node.next.next;
     }
 
+    /**
+     * 删除链表的倒数第N个节点
+     * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+     *
+     * 示例：
+     *
+     * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+     * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+     *
+     * 说明：
+     * 给定的 n 保证是有效的。
+     *
+     * 进阶：
+     * 你能尝试使用一趟扫描实现吗？
+     * @param head
+     * @param n
+     * @return
+     */
     public ListNode removeNthFromEnd(ListNode head, int n) {
         int i=1;
         ListNode node = head;
@@ -83,21 +124,94 @@ public class ListProblem {
         return head;
     }
 
+    /**
+     * 反转链表
+     * 反转一个单链表。
+     *
+     * 示例:
+     * 输入: 1->2->3->4->5->NULL
+     * 输出: 5->4->3->2->1->NULL
+     *
+     * 进阶:
+     * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr!=null){
+            ListNode nextNode = curr.next;
+            curr.next=prev;
+            prev= curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+
+    /**
+     * 合并两个有序链表
+     * 将两个有序链表合并为一个新的有序链表并返回。
+     * 新链表是通过拼接给定的两个链表的所有节点组成的。
+     *
+     * 示例：
+     * 输入：1->2->4, 1->3->4
+     * 输出：1->1->2->3->4->4
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = null;
+        ListNode curr = null;
+
+        while (l1!=null&&l2!=null){
+            ListNode append;
+            if (l1.val < l2.val) {
+                append=l1;
+                l1=l1.next;
+            }else{
+                append=l2;
+                l2=l2.next;
+            }
+            if (head == null) {
+                head=append;
+                curr=append;
+            }else{
+                curr.next=append;
+                curr= curr.next;
+            }
+        }
+        if (l1 != null && l2 == null) {
+            if (head == null) {
+                head=l1;
+            }else{
+                curr.next=l1;
+            }
+        } else if (l1 == null && l2 != null) {
+            if (head == null) {
+                head=l2;
+            }else{
+                curr.next=l2;
+            }
+        }
+        return head;
+    }
+
     public static void main(String[] args) {
-        LinkedList<String> o = new LinkedList<>();
-        o.add("s");
         ListProblem lp = new ListProblem();
-        System.out.println(lp.toString());
         lp.add(1);
-        System.out.println(lp.toString());
         lp.add(2);
-        System.out.println(lp.toString());
         lp.add(3);
-        System.out.println(lp.toString());
-        lp.add(4);
-        System.out.println(lp.toString());
-        lp.add(5);
-        System.out.println(lp.toString());
+
+        ListProblem lp2 = new ListProblem();
+        lp2.add(1);
+        lp2.add(3);
+        lp2.add(5);
+
+        ListProblem r = new ListProblem();
+        r.head=r.mergeTwoLists(lp.head,lp2.head);
+        System.out.println(r);
     }
 }
 
---
+++ primary_algorithms/src/test/java/ListProblemTest.java
@@ -0,0 +1,10 @@
+import array.ListProblem;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Jin Xiaofeng
+ */
+public class ListProblemTest {
+    private static ListProblem lp = new ListProblem();
+
+}
