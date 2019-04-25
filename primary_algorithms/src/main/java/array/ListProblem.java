3abe36160f3f9b06f08de15ff5739cc8cec2d15a
jinxf0822 <jinxf0822@foxmail.com>
2019-04-25 21:59:32 +0800

    判断回文链表

--- primary_algorithms/src/main/java/array/ListProblem.java
+++ primary_algorithms/src/main/java/array/ListProblem.java
@@ -1,6 +1,7 @@
 package array;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Jin Xiaofeng
@@ -217,6 +218,42 @@ public class ListProblem {
         ListProblem r = new ListProblem();
         r.head = r.mergeTwoLists(lp.head, lp2.head);
         System.out.println(r);
+    }
+
+    /**
+     * 回文链表
+     * 请判断一个链表是否为回文链表。
+     *
+     * 示例 1:
+     * 输入: 1->2
+     * 输出: false
+     *
+     * 示例 2:
+     * 输入: 1->2->2->1
+     * 输出: true
+     *
+     * 进阶：
+     * 你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+     * @param head
+     * @return
+     */
+    public boolean isPalindrome(ListNode head) {
+        List<ListNode> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head);
+            head=head.next;
+        }
+
+        if(list.size()<2){
+            return true;
+        }
+        for (int i = 0, j=list.size()-1; i < list.size()/2; i++,j--) {
+            if(list.get(i).val!=list.get(j).val){
+                return false;
+            }
+        }
+
+        return true;
     }
 
     /**
