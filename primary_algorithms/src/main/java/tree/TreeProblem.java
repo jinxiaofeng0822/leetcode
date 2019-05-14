d7384491bfe65aadb1681b2e19be8cdab4e09f1d
jinxf0822 <jinxf0822@foxmail.com>
2019-05-14 23:44:37 +0800

    二叉树的最大深度

---
+++ primary_algorithms/src/main/java/tree/TreeProblem.java
@@ -0,0 +1,63 @@
+package tree;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+/**
+ * @author Jin Xiaofeng
+ */
+public class TreeProblem {
+
+    public int maxDepth(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        HashMap<TreeNode,Integer> map = new HashMap<>();
+        int depth=1;
+        if (root != null) {
+            stack.push(root);
+            map.put(root,depth);
+        }else{
+            return 0;
+        }
+
+        while (stack.size()!=0){
+            TreeNode node = stack.pop();
+            int currentDepth = map.get(node);
+            depth = depth>currentDepth?depth:currentDepth;
+
+            if(node.left!=null){
+                stack.push(node.left);
+                map.put(node.left,currentDepth+1);
+            }
+            if(node.right!=null){
+                stack.push(node.right);
+                map.put(node.right,currentDepth+1);
+            }
+        }
+        return depth;
+    }
+
+    public static void main(String[] args) {
+        TreeNode t4 = new TreeNode(4,null,null);
+        TreeNode t2 = new TreeNode(2,t4,null);
+
+        TreeNode t5 = new TreeNode(5,null,null);
+        TreeNode t3 = new TreeNode(3,null,t5);
+
+        TreeNode t1 = new TreeNode(1, t2,t3);
+
+        TreeProblem tp = new TreeProblem();
+        int d = tp.maxDepth(t1);
+        System.out.println(d);
+    }
+}
+
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x, TreeNode left, TreeNode right){
+        val=x;
+        this.left=left;
+        this.right=right;
+    }
+}
