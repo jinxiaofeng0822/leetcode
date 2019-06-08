4e1ac1826631f781e4d8ed5da0db47a16784da03
jinxf0822 <jinxf0822@foxmail.com>
2019-06-08 18:38:23 +0800

    校验二叉搜索树

--- primary_algorithms/src/main/java/tree/TreeProblem.java
+++ primary_algorithms/src/main/java/tree/TreeProblem.java
@@ -1,5 +1,6 @@
 package tree;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Stack;
 
@@ -10,68 +11,90 @@ public class TreeProblem {
 
     public int maxDepth(TreeNode root) {
         Stack<TreeNode> stack = new Stack<>();
-        HashMap<TreeNode,Integer> map = new HashMap<>();
-        int depth=1;
+        HashMap<TreeNode, Integer> map = new HashMap<>();
+        int depth = 1;
         if (root != null) {
             stack.push(root);
-            map.put(root,depth);
-        }else{
+            map.put(root, depth);
+        } else {
             return 0;
         }
 
-        while (stack.size()!=0){
+        while (stack.size() != 0) {
             TreeNode node = stack.pop();
             int currentDepth = map.get(node);
-            depth = depth>currentDepth?depth:currentDepth;
+            depth = depth > currentDepth ? depth : currentDepth;
 
-            if(node.left!=null){
+            if (node.left != null) {
                 stack.push(node.left);
-                map.put(node.left,currentDepth+1);
+                map.put(node.left, currentDepth + 1);
             }
-            if(node.right!=null){
+            if (node.right != null) {
                 stack.push(node.right);
-                map.put(node.right,currentDepth+1);
+                map.put(node.right, currentDepth + 1);
             }
         }
         return depth;
     }
 
+    /**
+     * 验证二叉搜索树
+     * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+     * 假设一个二叉搜索树具有如下特征：
+     * 节点的左子树只包含小于当前节点的数。
+     * 节点的右子树只包含大于当前节点的数。
+     * 所有左子树和右子树自身必须也是二叉搜索树。
+     *
+     * @param root
+     * @return
+     */
     public boolean isValidBST(TreeNode root) {
-        if(root==null){
+        if (root == null) {
             return true;
         }
+        ArrayList<Integer> ar = new ArrayList<>();
         Stack<TreeNode> stack = new Stack<>();
-        stack.add(root);
-        while (!stack.isEmpty()){
-            TreeNode tn = stack.pop();
-            TreeNode left = tn.left;
-            TreeNode right = tn.right;
-            if(left!=null){
-                if (left.val < tn.val) {
-                    stack.push(left);
+        TreeNode temp=root;
+
+        //中序遍历
+        while (temp != null || stack.size() != 0) {
+            while(temp!=null){
+                stack.push(temp);
+                temp=temp.left;
+            }
+            if (stack.size() != 0) {
+                temp=stack.pop();
+                ar.add(temp.val);
+                if (temp.right != null) {
+                    temp=temp.right;
                 }else{
-                    return false;
+                    temp=null;
                 }
             }
-            if(right!=null){
-                if (right.val > tn.val) {
-                    stack.push(right);
-                }else{
-                    return false;
-                }
+        }
+        if (ar.size() == 1) {
+            return true;
+        }
+        int lastInt=ar.get(0);
+        for (int i = 1; i < ar.size(); i++) {
+            int now=ar.get(i);
+            if(now<=lastInt){
+                return false;
+            }else{
+                lastInt=now;
             }
         }
         return true;
     }
 
     public static void main(String[] args) {
-        TreeNode t4 = new TreeNode(4,null,null);
-        TreeNode t2 = new TreeNode(2,t4,null);
+        TreeNode t4 = new TreeNode(4, null, null);
+        TreeNode t2 = new TreeNode(2, t4, null);
 
-        TreeNode t5 = new TreeNode(5,null,null);
-        TreeNode t3 = new TreeNode(3,null,t5);
+        TreeNode t5 = new TreeNode(5, null, null);
+        TreeNode t3 = new TreeNode(3, null, t5);
 
-        TreeNode t1 = new TreeNode(1, t2,t3);
+        TreeNode t1 = new TreeNode(1, t2, t3);
 
         TreeProblem tp = new TreeProblem();
         int d = tp.maxDepth(t1);
@@ -79,13 +102,14 @@ public class TreeProblem {
     }
 }
 
-class TreeNode{
+class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
-    TreeNode(int x, TreeNode left, TreeNode right){
-        val=x;
-        this.left=left;
-        this.right=right;
+
+    TreeNode(int x, TreeNode left, TreeNode right) {
+        val = x;
+        this.left = left;
+        this.right = right;
     }
 }
