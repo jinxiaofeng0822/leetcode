7544793883ea3c89e311a400d112072283f11ff1
jinxf0822 <jinxf0822@foxmail.com>
2019-06-08 19:32:29 +0800

    校验镜像二叉树

--- primary_algorithms/src/main/java/tree/TreeProblem.java
+++ primary_algorithms/src/main/java/tree/TreeProblem.java
@@ -87,6 +87,39 @@ public class TreeProblem {
         return true;
     }
 
+    /**
+     * 给定一个二叉树，检查它是否是镜像对称的。
+     * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+     *
+     *     1
+     *    / \
+     *   2   2
+     *  / \ / \
+     * 3  4 4  3
+     *
+     * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+     *
+     *     1
+     *    / \
+     *   2   2
+     *    \   \
+     *    3    3
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root,root);
+    }
+
+    public boolean isMirror(TreeNode t1,TreeNode t2){
+        if (t1 == null && t2 == null) {
+            return true;
+        }
+        if (t1 == null || t2 == null) {
+            return false;
+        }
+        return t1.val==t2.val&&isMirror(t1.left,t2.right)&&isMirror(t1.right,t2.left);
+    }
     public static void main(String[] args) {
         TreeNode t4 = new TreeNode(4, null, null);
         TreeNode t2 = new TreeNode(2, t4, null);
