908197cfa8f55732f3a9a9b18f5b3bb3af135c1e
jinxf0822 <jinxf0822@foxmail.com>
2019-06-16 14:18:24 +0800

    将有序数组转换为二叉搜索树

--- primary_algorithms/src/main/java/tree/TreeProblem.java
+++ primary_algorithms/src/main/java/tree/TreeProblem.java
@@ -147,6 +147,35 @@ public class TreeProblem {
         }
         return levels;
     }
+
+    /**
+     * 将有序数组转换为二叉搜索树
+     *
+     * 将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
+     *
+     * 本题中，一个高度平衡二叉树是指一个二叉树每个节点
+     * 的左右两个子树的高度差的绝对值不超过 1。
+     * @param nums
+     * @return
+     */
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length == 0) {
+            return null;
+        }
+        return toBTS(nums,0,nums.length-1);
+    }
+
+    public TreeNode toBTS(int[] nums,int start,int end){
+        if (start>end) {
+            return null;
+        }
+        int mid=(start+end)/2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left=toBTS(nums,start,mid-1);
+        root.right=toBTS(nums,mid+1,end);
+        return root;
+    }
+
     public static void main(String[] args) {
         TreeNode t4 = new TreeNode(4, null, null);
         TreeNode t2 = new TreeNode(2, t4, null);
@@ -171,5 +200,9 @@ class TreeNode {
         val = x;
         this.left = left;
         this.right = right;
+    }
+
+    TreeNode(int x) {
+        val = x;
     }
 }
