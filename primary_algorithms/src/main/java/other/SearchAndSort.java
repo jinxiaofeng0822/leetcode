2bafd8275286aa0bc85798f780f7e03767e68b83
jinxf0822 <jinxf0822@foxmail.com>
2019-06-17 23:12:10 +0800

    第一个错误的版本

--- primary_algorithms/src/main/java/other/SearchAndSort.java
+++ primary_algorithms/src/main/java/other/SearchAndSort.java
@@ -51,11 +51,55 @@ public class SearchAndSort {
         }
     }
 
+    /**
+     * 第一个错误的版本
+     * 你是产品经理，目前正在带领一个团队开发新的产品。不幸的是，
+     * 你的产品的最新版本没有通过质量检测。由于每个版本都是基于
+     * 之前的版本开发的，所以错误的版本之后的所有版本都是错的。
+     * 假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有
+     * 版本出错的第一个错误的版本。
+     * 你可以通过调用 bool isBadVersion(version) 接口来判断
+     * 版本号 version 是否在单元测试中出错。实现一个函数来查找
+     * 第一个错误的版本。你应该尽量减少对调用 API 的次数。
+     *
+     * 示例:
+     *
+     * 给定 n = 5，并且 version = 4 是第一个错误的版本。
+     *
+     * 调用 isBadVersion(3) -> false
+     * 调用 isBadVersion(5) -> true
+     * 调用 isBadVersion(4) -> true
+     *
+     * 所以，4 是第一个错误的版本。
+     * @param n
+     * @return
+     */
+    public int firstBadVersion(int n) {
+        return fbv(0,n);
+    }
+
+    public int fbv(int start,int end){
+        if (end-start<=1) {
+            if(isBadVersion(start)){
+                return start;
+            }else{
+                return end;
+            }
+        }
+
+        int m=(end-start)/2+start;
+        if(isBadVersion(m)){
+            return fbv(start,m);
+        }else{
+            return fbv(m,end);
+        }
+    }
+
+    public boolean isBadVersion(int n){
+        return n>=1702766719;
+    }
     public static void main(String[] args) {
         SearchAndSort ss = new SearchAndSort();
-        int[] nums1={1,2,3,0,0,0};
-        int[] nums2={2,5,6};
-        ss.merge(nums1,3,nums2,3);
-        System.out.println(Arrays.toString(nums1));
+        System.out.println(ss.firstBadVersion(2126753390));
     }
 }
