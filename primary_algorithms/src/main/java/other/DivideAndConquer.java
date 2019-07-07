0487d756f5edb3bb77dcffe6b9f707820d448ca4
jinxf0822 <jinxf0822@foxmail.com>
2019-07-07 23:37:52 +0800

    最大自序和

--- primary_algorithms/src/main/java/other/DivideAndConquer.java
+++ primary_algorithms/src/main/java/other/DivideAndConquer.java
@@ -47,15 +47,56 @@ public class DivideAndConquer {
     }
 
     public int maxProfit(int[] prices) {
-        int minPrice=Integer.MAX_VALUE;
-        int maxProfit=0;
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
         for (int i = 0; i < prices.length; i++) {
             if (prices[i] < minPrice) {
-                minPrice=prices[i];
+                minPrice = prices[i];
             } else if (prices[i] - minPrice > maxProfit) {
-                maxProfit=prices[i] - minPrice;
+                maxProfit = prices[i] - minPrice;
             }
         }
         return maxProfit;
+    }
+
+    /**
+     * 最大子序和
+     * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组
+     * （子数组最少包含一个元素），返回其最大和。
+     * 示例:
+     * 输入: [-2,1,-3,4,-1,2,1,-5,4],
+     * 输出: 6
+     * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+     * 进阶:
+     * 如果你已经实现复杂度为 O(n) 的解法，
+     * 尝试使用更为精妙的分治法求解。
+     * @param nums
+     * @return
+     */
+    public int maxSubArray(int[] nums) {
+        //时间复杂度O(n^2),不好的算法
+        int max = Integer.MIN_VALUE;
+        int maxTemp;
+        for (int i = 0; i < nums.length; i++) {
+            maxTemp = nums[i];
+            int tempj=nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                tempj=tempj+nums[j];
+                if(tempj>maxTemp){
+                    maxTemp=tempj;
+                }
+            }
+            if (maxTemp > max) {
+                max = maxTemp;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] a={-2,1,-3,4,-1,2,1,-5,4};
+        DivideAndConquer d= new DivideAndConquer();
+        int max = d.maxSubArray(a);
+        System.out.println(max);
     }
 }
